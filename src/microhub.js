f6dac0055d7aa6bd1e7b36c95f0d980cca979fca
卓文理 <531840344@qq.com>
2018-06-09 22:16:12 +0800

    :sparkles: add word profile style

--- src/microhub.js
+++ src/microhub.js
@@ -4,58 +4,86 @@
  * @Date: 2018-06-09 12:01:15
  */
 
-// import 'metro4';
-// import clippy from './clippy';
 import './microhub.sass';
 
 const $body = $('body');
+const $header = $('.Header');
 const $graph = $('.js-contribution-graph');
-
-const COLOR = {
-    green: '#00ac00',
-    blue: '#00a7d9',
-    yellow: '#ffcf00',
-    red: '#ff0000',
-};
-
-window.clippy.load('Clippy', (agent) => {
-    agent.show();
-    agent.animate();
-
-    setTimeout(() => {
-        agent.speak('Whoops, it looks like you have some merge conflicts!');
-    }, 1500);
-});
 
 $body.addClass('microhub');
 
-$graph.find('rect').each((i, el) => {
-    const $this = $(el);
+function insertClippy() {
+    window.clippy.load('Clippy', (agent) => {
+        agent.show();
+        agent.animate();
 
-    switch ($this.attr('fill')) {
-    case '#c6e48b':
-        $this.attr('fill', COLOR.green);
-        break;
-    case '#7bc96f':
-        $this.attr('fill', COLOR.blue);
-        break;
-    case '#239a3b':
-        $this.attr('fill', COLOR.yellow);
-        break;
-    case '#196127':
-        $this.attr('fill', COLOR.red);
-        break;
-    default:
-        break;
+        setTimeout(() => {
+            agent.speak('Whoops, it looks like you have some merge conflicts!');
+        }, 1500);
+    });
+}
+
+function changeContributionGraph() {
+    const COLOR = {
+        green: '#00ac00',
+        blue: '#00a7d9',
+        yellow: '#ffcf00',
+        red: '#ff0000',
+    };
+
+    $graph.find('rect').each((i, el) => {
+        const $this = $(el);
+
+        switch ($this.attr('fill')) {
+        case '#c6e48b':
+            $this.attr('fill', COLOR.green);
+            break;
+        case '#7bc96f':
+            $this.attr('fill', COLOR.blue);
+            break;
+        case '#239a3b':
+            $this.attr('fill', COLOR.yellow);
+            break;
+        case '#196127':
+            $this.attr('fill', COLOR.red);
+            break;
+        default:
+            break;
+        }
+    });
+
+    $graph.find('ul.legend').html(`
+        <li style="background-color: #eee"></li>
+        <li style="background-color: ${COLOR.green}"></li>
+        <li style="background-color: ${COLOR.blue}"></li>
+        <li style="background-color: ${COLOR.yellow}"></li>
+        <li style="background-color: ${COLOR.red}"></li>
+    `);
+}
+
+function insertWordToolbar() {
+    let isHide = !!window.localStorage.getItem('MICROHUB:HIDE_WORD_TOOLBAR');
+    const $toolbar = $('<div class="word-toolbar"><div class="ctrl"></div></div>');
+    $body.addClass('show-word-toolbar');
+    $header.append($toolbar);
+
+    if (!isHide) {
+        $toolbar.addClass('active');
+        $body.addClass('active-word-toolbar');
     }
-});
 
-$graph.find('ul.legend').html(`
-    <li style="background-color: #eee"></li>
-    <li style="background-color: ${COLOR.green}"></li>
-    <li style="background-color: ${COLOR.blue}"></li>
-    <li style="background-color: ${COLOR.yellow}"></li>
-    <li style="background-color: ${COLOR.red}"></li>
-`);
+    $toolbar.delegate('.ctrl', 'click', () => {
+        $toolbar.toggleClass('active');
+        $body.toggleClass('active-word-toolbar');
+        window.localStorage.setItem('MICROHUB:HIDE_WORD_TOOLBAR', isHide ? '' : 1);
+        isHide = !isHide;
+    });
+}
 
-window.chrome = chrome;
+function init() {
+    insertClippy();
+    changeContributionGraph();
+    insertWordToolbar();
+}
+
+init();
